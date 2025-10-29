5e025676b2b31d6295fdbf9e13b8cc323e8700aa
Gangzi-Jia <97003699+Gangzi-Jia@users.noreply.github.com>
2025-10-29 15:21:16 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,12 @@
 (function($){
   $(function(){
     $(".button-collapse").sideNav();
+    
+    // 处理侧边栏菜单的展开/收起
+    $('.collapsible-header').on('click', function(e) {
+      e.preventDefault();
+      $(this).toggleClass('active');
+      $(this).next('.collapsible-body').slideToggle(300);
+    });
   });
 })(jQuery);
